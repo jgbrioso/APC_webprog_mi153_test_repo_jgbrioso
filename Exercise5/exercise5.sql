fa3441379a169fc4e71cace4b5c070c05b9872b0
Job Brioso <briosojob@gmail.com>
2016-12-16 12:35:29 +0800

    edited

--- Exercise5/exercise5.sql
+++ Exercise5/exercise5.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE `dbtuts` ;
 CREATE TABLE `dbtuts`.`users` ( `user_id` INT( 10 ) NOT NULL AUTO_INCREMENT PRIMARY KEY , 
 `Name` VARCHAR( 25 ) NOT NULL , `Email` VARCHAR( 25 ) NOT NULL , 
-`HomeAddress` VARCHAR( 45 ) NOT NULL,
-`CellNo` INT( 11 ),
-`Gender` VARCHAR( 45 ) NOT NULL, 
+`Email` VARCHAR( 45 ) NOT NULL,
+`Gender` VARCHAR( 11 ) NOT NULL,
 `Comment` VARCHAR( 45 )) ENGINE = InnoDB 
